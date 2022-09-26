9ea3628bf1db15f6ce0ffcbd0cf7a44c002a0906
Yuki Minoh <yukikaze.0511@gmail.com>
2022-09-27 06:25:45 +0900

    revert typing

--- Tensho_InfoVAE/models/types_.py
+++ Tensho_InfoVAE/models/types_.py
@@ -1,4 +1,4 @@
-# from typing import List, Callable, Union, Any, TypeVar, Tuple
+from typing import List, Callable, Union, Any, TypeVar, Tuple
 # from torch import tensor as Tensor
 from torch import Tensor
 
